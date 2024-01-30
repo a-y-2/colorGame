686aa8e81a78ccee120dd380302399e321472587
Ayushi Prasad <60753222+a-y-2@users.noreply.github.com>
2024-01-30 10:50:43 +0530

    Update color.js

--- color.js
+++ color.js
@@ -70,7 +70,7 @@ for(var i=0;i<squares.length;i++){
 	 	var clickedcolor=this.style.background;
 		if(clickedcolor===pickedcolor){
 	 		messagedisplay.textContent="correct :)"
-	 		resetbutton.textContent="to play again,please click here";
+	 		resetbutton.textContent="to play again,please  click here";
 	 		changecolors(clickedcolor);
 	 		h1.style.background=clickedcolor;
 		}
